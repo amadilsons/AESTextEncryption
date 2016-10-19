29f02351c02b41f0600b05aa9a38c773079e3959
amadilsons <joao.amado.95@gmail.com>
2016-10-20 02:11:22 +0800

    edit EmailHandler.java Important comments

--- src/EmailHandler.java
+++ src/EmailHandler.java
@@ -1,5 +1,40 @@
+package aestextencryption;
 /**
  * Created by João Amado on 17/10/2016.
+ * Implement addTextMessage, addFile
+ * Constructor builds email object
+ * Mails sent though gmail smtp server
  */
+
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 public class EmailHandler {
+    private static String host = "l";
+
+    public EmailHandler(){
+
+        //Set javax.mail properties
+        Properties mail_props = new Properties();
+        mail_props.put("mail.smtp.auth", "true");
+        mail_props.put("mail.smtp.host", "smtp.gmail.com");
+        mail_props.put("mail.smtp.port", "587");
+        mail_props.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(mail_props,
+                new javax.mail.Authenticator() {
+                    //Creat class extending javax.mail.Authenticator() to override method getPasswordAuthentication()
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+    }
 }
